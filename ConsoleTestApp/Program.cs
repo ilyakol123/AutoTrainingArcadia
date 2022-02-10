088b2354c6fe6980f7e80af735b62e34b655fda0
ilyakol123 <ilja-kolesnikov@mail.ru>
2022-02-10 12:06:27 +0100

    Add project files.

---
+++ ConsoleTestApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(3 + 5);
+        }
+    }
+}

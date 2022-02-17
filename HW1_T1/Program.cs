701f7f342a4dda4e457831a20b462cc100cf787d
ilyakol123 <ilja-kolesnikov@mail.ru>
2022-02-17 19:18:28 +0100

    Homework1, Task1 added

--- HW1_T1/Program.cs
+++ HW1_T1/Program.cs
@@ -6,7 +6,13 @@ namespace HW1_T1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = Int32.Parse(Console.ReadLine());
+            int y = Int32.Parse(Console.ReadLine());
+            int z = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("First = " + (x + y) * (z * z + 1));
+            Console.WriteLine("Second = " + ((x % z) - 1) * (Math.Sqrt(y)));
+            Console.WriteLine("Third = " + (((x * y) + (y * z)) / (Math.Pow(z, 3))));
         }
     }
 }

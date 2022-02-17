b3e7fcb0e79ab9407776ab5f69fce799a4322bd9
ilyakol123 <ilja-kolesnikov@mail.ru>
2022-02-17 19:19:40 +0100

    Homework1, Task 2 added

---
+++ HW1_T2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HW1_T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = Double.Parse(Console.ReadLine());
+            double hight = Double.Parse(Console.ReadLine());
+            double length = Double.Parse(Console.ReadLine());
+            double diagonalLength = Math.Pow(width, 2) + Math.Pow(hight, 2) + Math.Pow(length, 2);
+
+            Console.WriteLine("Diagonal length = " + diagonalLength);
+        }
+    }
+}

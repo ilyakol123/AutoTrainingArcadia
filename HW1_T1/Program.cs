0a8b00a04fb5e8b73417baba52b8f10e5d4b5688
ilyakol123 <ilja-kolesnikov@mail.ru>
2022-02-18 09:54:48 +0100

    fix: task 1 fix Added text for user, to enter numbers Changed number types from int to double

--- HW1_T1/Program.cs
+++ HW1_T1/Program.cs
@@ -6,9 +6,12 @@ namespace HW1_T1
     {
         static void Main(string[] args)
         {
-            int x = Int32.Parse(Console.ReadLine());
-            int y = Int32.Parse(Console.ReadLine());
-            int z = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the \"x\" num");
+            double x = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the \"y\" num");
+            double y = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the \"z\" num");
+            double z = Double.Parse(Console.ReadLine());
 
             Console.WriteLine("First = " + (x + y) * (z * z + 1));
             Console.WriteLine("Second = " + ((x % z) - 1) * (Math.Sqrt(y)));

270e845be7ad229e318e222e170ccebf02f779db
ilyakol123 <ilja-kolesnikov@mail.ru>
2022-02-18 09:57:05 +0100

    fix: task 2 fixed Added text for user inputs Added Sqrt for diagonal length

--- HW1_T2/Program.cs
+++ HW1_T2/Program.cs
@@ -6,10 +6,16 @@ namespace HW1_T2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the width");
             double width = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the height");
             double hight = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the length");
             double length = Double.Parse(Console.ReadLine());
-            double diagonalLength = Math.Pow(width, 2) + Math.Pow(hight, 2) + Math.Pow(length, 2);
+
+            double diagonalLength = Math.Sqrt(Math.Pow(width, 2) + Math.Pow(hight, 2) + Math.Pow(length, 2));
 
             Console.WriteLine("Diagonal length = " + diagonalLength);
         }

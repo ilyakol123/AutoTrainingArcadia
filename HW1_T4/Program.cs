c53627a4708a114e5d94850e5d2883dbb39becf9
ilyakol123 <ilja-kolesnikov@mail.ru>
2022-02-24 23:03:56 +0100

    Task 4 added

---
+++ HW1_T4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HW1_T4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first line for concatination");
+            string lineInput_1 = Console.ReadLine();
+            Console.WriteLine("Enter second line for concatination");
+            string lineInput_2 = Console.ReadLine();
+            Console.WriteLine("Enter the symbol for concatination");
+            char charInput = (char)Console.Read();
+
+            string concatString = lineInput_1 + lineInput_2 + charInput.ToString();
+
+            Console.WriteLine($"Index of {charInput} in {concatString}: {concatString.LastIndexOf(charInput)}");
+
+        }
+    }
+}

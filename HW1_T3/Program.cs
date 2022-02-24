61bb41e47be4b325bebe0366780c73078e1f22f5
ilyakol123 <ilja-kolesnikov@mail.ru>
2022-02-24 22:51:48 +0100

    Task 3 added

---
+++ HW1_T3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HW1_T3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first line");
+            string input1 = Console.ReadLine().ToUpper();
+            Console.WriteLine("Enter second line");
+            string input2 = Console.ReadLine().ToUpper();
+            Console.WriteLine("Enter third line");
+            string input3 = Console.ReadLine().ToUpper();
+            bool answer1;
+            bool answer2;
+            if (input1.StartsWith(input2))
+            {
+                answer1 = true;
+            }
+            else
+            {
+                answer1 = false;
+            }
+
+            if (input1.EndsWith(input3))
+            {
+                answer2 = true;
+            }
+            else
+            {
+                answer2 = false;
+            }
+            Console.WriteLine($"String {input1} starts from {input2}: {answer1}:");
+            Console.WriteLine($"String {input1} ends with {input3}: {answer2}:");
+        }
+    }
+}

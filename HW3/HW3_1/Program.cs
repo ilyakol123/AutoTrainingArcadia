2e3ada185f60470ebbd459d1ec3276826f0b625a
ilyakol123 <ilja-kolesnikov@mail.ru>
2022-04-15 11:47:08 +0200

    Branch rename

--- HW3/HW3_1/Program.cs
+++ HW3/HW3_1/Program.cs
@@ -6,7 +6,7 @@ namespace HW3_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World123!");
         }
     }
 }
